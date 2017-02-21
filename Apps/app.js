6c3337b2884e14cb73c8bfbcaf4c2bc3c63d3b18
Bao Nguyen <baonguyen.work@gmail.com>
2017-02-21 16:24:37 +0700

    Restructure index.ios.js and create a simple component to see how it works

---
+++ Apps/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "React";
+import {AppRegistry,  Text, View} from "react-native"
+
+export default class Dashboard extends Component{
+  render(){
+    return (
+      <View style={{margin:50}}>
+        <Text>Fck ya !</Text>
+      </View>
+    )
+  }
+}
+
+module.exports = Dashboard;
